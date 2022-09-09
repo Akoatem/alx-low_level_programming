f4d801a95295c799812009f9cf7362b0cd9a4400
Akoatem <akoatemsam2003@gmail.com>
2022-09-09 13:19:27 +0200

    Alphabet characters

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * msin - To print number in alphabet
+ * Description: print in lowercase letters
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	for (char c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+	return (0);
+}
