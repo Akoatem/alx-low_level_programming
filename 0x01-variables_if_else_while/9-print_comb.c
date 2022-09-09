0742504db58f6abac3b7a5b8818318033f94768e
Akoatem <akoatemsam2003@gmail.com>
2022-09-09 23:00:59 +0200

    updated

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,10 +10,10 @@ int main(void)
 {
 	int i;
 
-	for (i = 48; i < 58; i++)
+	for (i = '0'; i <= '9' ; i++)
 	{
 		putchar(i);
-		if (i != 57)
+		if (i != '9')
 		{
 			putchar(',');
 			putchar('');
