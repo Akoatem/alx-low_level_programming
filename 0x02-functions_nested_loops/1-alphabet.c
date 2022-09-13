9f6c22aa101251f5c7fa6a92a67c0af6cddba283
akoatemsam2003@gmail.com <akoatemsam2003@gmail.com>
2022-09-14 00:46:54 +0200

    Alphabet letters

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - Print out alphabet
+ * Description: Alphaphet print in order
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void);
+{
+	int i;
+
+	for (i = 'a'; i < 'z'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar("\n");
+	return (0);
+}
