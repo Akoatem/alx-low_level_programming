a1cca567c5dcd278f462fc76601debb70302e4c8
akoatemsam2003@gmail.com <akoatemsam2003@gmail.com>
2022-09-14 23:00:08 +0200

    Hot fix Timetable

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- *  times_table - prints the 9 times table
- *  Description: prints the 9 times table
+ * times_table - prints the 9 times table
+ * Description: prints the 9 times table
  *
- *  Return: void
+ * Return: void
  */
 
 void times_table(void)
@@ -13,28 +13,31 @@ void times_table(void)
 
 	for (row = 0; row <= 9; row++)
 	{
-		product = row * column;
-		tens = product / 10;
-		ones = product % 10;
+		for (column = 0; column <= 9; column++)
+		{
+			product = row * column;
+			tens = product / 10;
+			ones = product % 10;
 
-		if (column == 0)
-		{
-			_putchar('0');
+			if (column == 0)
+			{
+				_putchar('0');
+			}
+			else if (product < 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(ones + '0');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(tens + '0');
+				_putchar(ones + '0');
+			}
 		}
-		else if (product < 10)
-		{
-			_putchar(',');
-			_putchar(' ');
-			_putchar(' ');
-			_putchar(ones + '0');
-		}
-		else
-		{
-			_putchar(',');
-			_putchar(' ');
-			_putchar(tens + '0');
-			_putchar(ones + '0');
-		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
