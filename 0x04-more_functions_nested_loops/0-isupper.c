08dabda7765d13890d7a8dcc4ac6a6606749b9c4
akoatemsam2003@gmail.com <akoatemsam2003@gmail.com>
2022-09-15 13:04:51 +0200

    hot fix

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -5,8 +5,8 @@
 /**
  * _isupper - A function that checks for uppercase character.
  * @c: An input character
- * return---> non zero --->character is uppercase
- * return---> zero --->character is not uppercase
+ *
+ * Return: 1 if c is uppercase or 0 otherwise
  */
 
 int _isupper(int c)
