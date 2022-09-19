1d39d30bbd38019ff300e821e08a34f20f26d22f
akoatemsam2003@gmail.com <akoatemsam2003@gmail.com>
2022-09-19 15:28:52 +0200

    hot fix

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,20 +8,16 @@
 
 void puts_half(char *str)
 {
-	int i, j, max;
+	int len = 0, i, n;
 
-	i = 0;
-	while (str[i] != '\0')
-	{
-		i++;
-	}
-	max = i;
-	j = max / 2;
-	while (j <= max)
-	{
-		_putchar(str[j]);
-		j++;
-	}
+	while (str[len] != '\0')
+		len++;
+	if (len % 2 == 0)
+		n = len / 2;
+	else
+		n = (len + 1) / 2;
+	for (i = n; i < len; i++)
+		_putchar(str[i]);
 	_putchar('\n');
 }
 
