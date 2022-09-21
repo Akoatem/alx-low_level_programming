966b2f1ce7c411e882e826b33154f515d6b96d15
akoatemsam2003@gmail.com <akoatemsam2003@gmail.com>
2022-09-21 19:13:53 +0200

    great fix

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,4 +1,12 @@
+/*
+ * File: 102-infinite_add.c
+ */
+
+
 #include "main.h"
+
+char *add_strings(char *n1, char *n2, char *r, int r_index);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
 
 /**
  * infinite_add - C function that adds two numbers stored
@@ -18,6 +26,35 @@
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
+	int num, tens = 0;
+
+	for (; *n1 && *n2; n1--, n2--, r_index--)
+	{
+		num = (*n1 - '0') + (*n2 - '0');
+		num += tens;
+		*(r + r_index) = (num % 10) + '0';
+		tens = num / 10;
+	}
+	for (; *n1; n1--, r_index--)
+	{
+		num = (*n1 - '0') + tens;
+		*(r + r_index) = (num % 10) + '0';
+		tens = num / 10;
+	}
+	for (; *n2; n2--, r_index--)
+	{
+		num = (*n2 - '0') + tens;
+		*(r + r_index) = (num % 10) + '0';
+		tens = num / 10;
+	}
+	if (tens && r_index >= 0)
+	{
+		*(r + r_index) = (tens % 10) + '0';
+		return (r + r_index);
+	}
+	else if (tens && r_index < 0)
+		return (0);
+	return (r + r_index + 1);
 }
 /**
  * infinite_add - C function that
@@ -30,28 +67,19 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
  * Return:if r can store the sum - a pointer to the result.
  *        if r cannot store the sum - 0.
  */
-
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int num, tens = 0;
+	int index, n1_len = 0, n2_len = 0;
 
-	for (; *n1 && *n2; n1--, r_index--)
-	{
-		num = (*n1 - '0') + (*n - '0');
-		num += tens;
-		*(r + r_index) = (num % 10) + '0';
-		tens = num / 10;
-	}
-	for (; *n1; n1--; r_index++)
-	{
-		num = *(n1 - '0') + tens;
-		*(r + r_index) = (num % 10) + '0';
-		tens = num / 10;
-	}
-	for (; *n2; n2--; r_index--)
-	{
-		num = (*n2 - '0') + tens;
-		*(r + r_index) = (num % 10) + '0';
-		tens = num / 10;
-	}
+	for (index = 0; *(n1 + index); index++)
+		n1_len++;
+	for (index = 0; *(n2 + index); index++)
+		n2_len++;
+	if (size_r <= n1_len + 1 || size_r <= n2_len + 1)
+		return (0);
+	n1 += n1_len - 1;
+	n2 += n2_len - 1;
+	*(r + size_r) = '\0';
+	return (add_strings(n1, n2, r, --size_r));
 }
+
