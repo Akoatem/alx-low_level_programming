4b19e5023cd876e20c45cef2e8e0dc0c480f3312
akoatemsam2003@gmail.com <akoatemsam2003@gmail.com>
2022-09-21 18:32:19 +0200

    hot fix

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,33 +8,20 @@
 
 char *cap_string(char *s)
 {
-	int i = 0;
+	int a = 0, i;
+	int cspc = 13;
+	char spc[] = {32, '\t', '\n', 44, ';', 46, '!', '?', '"', '(', ')', '{', '}'};
 
-	while (s[i])
+	while (s[a])
 	{
-		if (i == 0 && (s[i] >= 'a' && s[i] <= 'z'))
-			s[i] -= 32;
-		if (check_seperators(s[i]) && (s[i + 1] >= 'a' && s[i + 1] <= 'z'))
-			s[i + 1] -= 32;
-		i++;
+		i = 0;
+		while (i < cspc)
+		{
+			if ((a == 0 || s[a - 1] == spc[i]) && (s[a] >= 97 && s[a] <= 122))
+				s[a] -= 32;
+			i++;
+		}
+		a++;
 	}
 	return (s);
-}
-/**
- * check_seperators - Separators of words: space, tabulation, new line,
- *  ,, ;, ., !, ?, ", (, ), {, and }
- *  @c: an input character.
- *  Return: 1 if seperator, 0 otherwise.
- */
-int check_seperators(char c)
-{
-	int i = 0;
-	char seperators[13] = { ' ', '\t', '\n', ',', ';', '.', '!', '?',
-		'"', '(', ')', '{', '}' };
-	for (; i < 13; i++)
-	{
-		if (c == seperators[i])
-			return (1);
-	}
-	return (0);
 }
