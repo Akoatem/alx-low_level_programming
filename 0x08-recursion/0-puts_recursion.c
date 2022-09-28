27f2c6753b6be4c2b9fd791ec05f7d79dea6eb4e
Akoatem <akoatemsam2003@gmail.com>
2022-09-28 15:18:02 +0200

    hot fix

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -11,11 +11,11 @@ void _puts_recursion(char *s)
 {
 	if (*s == '\0')
 	{
-		_putchar('\n');
+		_putchar(*s);
+		_puts_recursion(s + 1);
 	}
 	else
 	{
-		_putchar(s[0]);
-		_puts_recursion(s + 1);
+		_putchar('\n');
 	}
 }
