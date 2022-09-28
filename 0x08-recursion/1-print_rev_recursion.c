08e7530cb38aa59124bebaefc063eca4d2c78d8d
Akoatem <akoatemsam2003@gmail.com>
2022-09-28 17:05:47 +0200

    print string in reverse order

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - prints a string in reverse.
+ * @s: input string
+ *
+ * Return: Nothing
+ */
+
+void _print_rev_recursion(char *s)
+{
+	/* s = "ako\0";*/
+	if (*s == '\0')
+	{
+		return;
+		s++;
+		_print_rev_recursion(s);
+		s--;
+		_putchar(*s);
+	}
+}
