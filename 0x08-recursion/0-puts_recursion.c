4b7e613bb4c3b351f607808c93d5c1e83a808f7c
Akoatem <akoatemsam2003@gmail.com>
2022-09-28 16:01:20 +0200

    task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -9,11 +9,13 @@
 
 void _puts_recursion(char *s)
 {
-	if (*s)
+	/*s = "ako\0"*/
+	if (*s == '\0')
 	{
-		_putchar(*);
-		_puts_recursion(s + 1);
-	}
-	else
 		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	s++;
+	_puts_recursion(s);
 }
