18b9f68741da3f81c7e9846fbae5b2fecdedfb47
Akoatem <akoatemsam2003@gmail.com>
2022-09-30 19:07:49 +0200

    pass all argument

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -11,9 +11,11 @@ int main(int argc, char *argv[])
 {
 	int i;
 
-	for (i = 0; i < argc; i++)
+	i = 0;
+	while (i < argc)
 	{
-		printf("%s\n", argc[i]);
+		printf("%s\n", argv[i]);
+		i++;
 	}
 	return (0);
 }
