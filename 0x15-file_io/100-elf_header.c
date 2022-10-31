15bd2a3eb5f51e20588b506139ebe48af31083b7
Akoatem <akoatemsam2003@gmail.com>
2022-10-31 19:49:39 +0200

    Write a program that displays the information contained in the ELF header at the start of an ELF file.

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,14 +1,47 @@
 #include <stdio.h>
 #include "main.h"
 
+void check_elf(unsigned char *e_ident);
+void print_magic(unsigned char *e_ident);
+void print_class(unsigned char *e_ident);
+void print_data(unsigned char *e_ident);
+void print_version(unsigned char *e_ident);
+void print_abi(unsigned char *e_ident);
+void print_osabi(unsigned char *e_ident);
+void print_type(unsigned int e_type, unsigned char *e_ident);
+void print_entry(unsigned long int e_entry, unsigned char *e_ident);
+void close_elf(int elf);
+
+/**
+  *check_elf - checks if a file is an ELF file.
+  *@e_ident: a pointer to an array containing the ELF magis number
+  *Description: if the file is not ELF file exit 98
+  */
+void check_elf(unsigned char *e_ident)
+{
+	int index;
+
+	for (index = 0; index < 4; index++)
+	{
+		if (e_ident[index] != 127 &&
+		    e_ident[index] != 'E' &&
+		    e_ident[index] != 'L' &&
+		    e_ident[index] != 'F')
+		{
+			dprintf(STDERR_FILENO, "Error: not an ELF file\n");
+			exit(98);
+		}
+	}
+}
+
 /**
  * main - The entry point for program to get header of ELF file
  * @argc: The number of arguments
  * @argv: The pointer to array of arguments
  * Return: 1 on success, error code on failure
  */
-int main(int argc, char *argv[])
+/**int main(int argc, char *argv[])
 {
 	printf("argc:%d, argv:%p\n", argc, (void *)*argv);
 	return (1);
-}
+}*/
